777c95cf84acdc25d089d12fba8d843b1d4afe88
rubenangel5891 <rubenangel5891@gmail.com>
2019-05-10 12:38:25 -0400

    avence de decha 10/05/2019

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <html>
     <head><meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ /> </head>
     <body>
-        <p>Hola ñ í á ó ú</p>
-
 <?php
 
 $host = '{Microsoft Access Driver (*.mdb)}';
@@ -23,42 +21,42 @@ try {
     $pdo = new PDO($dsn, $user, $pass, $options);
      
     
-    
-    
-    
-    
-
-    
+  
    /******* CONSULTA SELECT ***************/
-echo "******* PDO::query()**********<br/>";
+echo "******* INFOCAL POTOSÍ - REGISTRO DE DOCUMENTACIÓN **********<br/>";
     $stmt = $pdo->query("SELECT TC1.IdCursoC,TC1.Area, TC1.Curso,TC1.Costo,TC1.FECHAI, TC1.Horario, TC1.Estado
                          FROM CCursos TC1
                          WHERE TC1.Area=7 AND TC1.FECHAI > #01/01/2019# AND TC1.Estado='I'ORDER BY TC1.Curso");
     
-    //$Cursos='';
-    $cont1=1;
+  
+    $cont1=0;
+    unset($Curso);
+    $Curso=array();
     while ($row = $stmt->fetch())
     {
-         echo utf8_encode($row['IdCursoC']) . "<br/>";
-        $cursos=array(
-            array(  'IdCursoC' => utf8_encode($row['IdCursoC']),
-                    'CursoC' => utf8_encode($row['CursoC']),
-                    'Costo' => (int) utf8_encode($row['Costo']), 
-                    'Estado' => utf8_encode ($row['Estado'])
-                )
-            );
-         
-         $cont1++;
-    }
-  
-   
-
-                                                        $Curso1["IdCursoC"][$cont1]=utf8_encode ($fila->IdCursoC); 
-                                                            $Curso1["Curso"][$cont1]=utf8_encode ($fila->Curso); 
-                                                            $Curso1["Costo"][$cont1]=(int)utf8_encode ($fila->Costo); 
-                                                            $Curso1["Estado"][$cont1]=utf8_encode ($fila->Estado); 
-
+        ++$cont1;
+       $Curso[$cont1]["IdCursoC"]= utf8_encode($row['IdCursoC']); 
+       $Curso[$cont1]["Curso"]=utf8_encode($row['Curso']); 
+       $Curso[$cont1]["Costo"]= (int) utf8_encode($row['Costo']); 
+       $Curso[$cont1]["Estado"]=utf8_encode ($row['Estado']);     
+    }      
     
+?>
+<form name="formulario" method="post" action="index.php"> 
+    <?= $Curso[2]['IdCursoC']." | ".$Curso[2]['Curso']." | ".$Curso[2]['Costo']. "<br/>";?>
+    <input type="submit" name="Consulta" value="Generar Reporte" />
+</form>  
+             
+        
+<?php
+echo filter_input (INPUT_POST,'Consulta',FILTER_SANITIZE_STRING);
+
+      if( null!= filter_input (INPUT_POST,'Consulta',FILTER_SANITIZE_STRING))
+        {
+            echo " proceso ...";
+        }  
+
+
 } catch (\PDOException $e) {
      throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
